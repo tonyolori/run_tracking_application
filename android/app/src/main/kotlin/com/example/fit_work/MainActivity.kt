9a124ece1a18c2b0c5c982099dc9f466d04381d9
tonyolori <anthonyolori123@gmail.com>
2023-01-23 16:09:21 +0000

    project update

---
+++ android/app/src/main/kotlin/com/example/fit_work/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fit_work
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+//import 'dart:html';
+// ignore_for_file: avoid_print
+
+import 'package:fit_work/activity_recognition.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'run_tracking.dart';
+import 'package:flutter/material.dart';
+import 'map_tracking.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Pedometer example app'),
+      ),
+      body: OrderTrackingPage(),
+    ));
+  }
+}
+
+class PermissionAsking extends StatefulWidget {
+  const PermissionAsking({super.key});
+
+  @override
+  State<PermissionAsking> createState() => _PermissionAskingState();
+}
+
+class _PermissionAskingState extends State<PermissionAsking> {
+  askPermission() async {
+    // var status = await Permission.camera.status;
+    var status = await Permission.activityRecognition.request();
+
+    print(status);
+//     if (status.isDenied) {
+//       // We didn't ask for permission yet or the permission has been denied before but not permanently.
+//     }
+
+// // You can can also directly ask the permission about its status.
+//     //   if (await Permission.location.isRestricted) {
+//     //     // The OS restricts access, for example because of parental controls.
+//     //   }
+//     //   // PermissionStatus status = await Permission.contacts.request();
+//     //   // if(status .isDenied == true)
+//     //   //   {
+//     //   //     askPermission();
+//     //   //   }
+//     //   // else
+//     //   //   {
+//     //   //     return true;
+//     //   //   }
+//     print('status =');
+//     print(status);
+//     return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: const Text('Open route'),
+        onPressed: () async {
+          await askPermission();
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => runTracking()),
+          );
+          //  Navigate to second route when tapped.
+        });
+  }
+}
