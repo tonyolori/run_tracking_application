154e2a865d70415f1bab700e91b6d0a904764005
tonyolori <anthonyolori123@gmail.com>
2023-03-22 17:19:28 +0200

    centered step page widget

--- lib/pages/Step_page.dart
+++ lib/pages/Step_page.dart
@@ -35,91 +35,104 @@ class _RunTrackingState extends State<RunTracking> {
     //print(((int.parse(_steps)) ~/ 80 ));
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: [
-                  Text(
-                    "Steps",
-                    style: TextStyle(fontSize: 48),
-                  ),
-                  OutlinedButton(
-                    onPressed: () async {},
-                    child: Text("press me"),
-                  )
-                ],
-              ),
-              SizedBox(
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: [
+                Text(
+                  "Steps",
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: SizedBox(
                 height: kBoxSize,
                 width: kBoxSize,
-                child: Stack(
-                  children: [
-                    CircularStepProgressIndicator(
-                      totalSteps: 100,
-                      currentStep: ((int.parse(_steps)* 100) ~/ 8000 ),
-                      stepSize: 15,
-                      selectedColor: Colors.greenAccent,
-                      unselectedColor: Colors.grey[200],
-                      circularDirection: CircularDirection.clockwise,
-                      startingAngle: kPI - kPI / 4, //4.71239,
-                      arcSize: kPI * 1.5,
-                      padding: 0,
-                      width: kBoxSize,
-                      height: kBoxSize,
-                      selectedStepSize: 15,
-                      roundedCap: (_, __) => true,
-                    ),
-                    Center(
-                      child: Text(
-                        context.watch<StepHelper>().steps,
-                        style: TextStyle(
-                          height: null,
-                          fontSize: 70,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 20,
-                        left: 70,
-                        right: 70,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              association(
-                                label: "Duration",
-                                value: "5H",
-                              ),
-                              association(
-                                label: "Steps",
-                                value: _steps,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                child: StepWidget(steps: _steps),
               ),
+            ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //     SmallIcon(),
+            //     SmallIcon(),
+            //   ],
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StepWidget extends StatelessWidget {
+  const StepWidget({
+    super.key,
+    required String steps,
+  }) : _steps = steps;
+
+  final String _steps;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CircularStepProgressIndicator(
+          totalSteps: 100,
+          currentStep: ((int.parse(_steps)* 100) ~/ 8000 ),
+          stepSize: 15,
+          selectedColor: Colors.greenAccent,
+          unselectedColor: Colors.grey[200],
+          circularDirection: CircularDirection.clockwise,
+          startingAngle: kPI - kPI / 4, //4.71239,
+          arcSize: kPI * 1.5,
+          padding: 0,
+          width: kBoxSize,
+          height: kBoxSize,
+          selectedStepSize: 15,
+          roundedCap: (_, __) => true,
+        ),
+        Center(
+          child: Text(
+            context.watch<StepHelper>().steps,
+            style: TextStyle(
+              height: null,
+              fontSize: 70,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 20,
+            left: 70,
+            right: 70,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SmallIcon(),
-                  SmallIcon(),
+                  association(
+                    label: "Duration",
+                    value: "5H",
+                  ),
+                  association(
+                    label: "Steps",
+                    value: _steps,
+                  ),
                 ],
-              )
-              //ElevatedButton(onPressed: onPressed, child: child)
+              ),
             ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
